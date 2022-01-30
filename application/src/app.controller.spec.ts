a98d846ed871a8015a8fda4932dec5a7cba39731
Philip Damra <pdamra@dealerinspire.com>
2022-01-29 21:45:10 -0700

    Ensuring failing tests will stop the build

--- application/src/app.controller.spec.ts
+++ application/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Note Taker!"', () => {
+      expect(appController.getHello()).toBe('Note Taker!');
     });
   });
 });
