3e630bb9bf73e3c46d2e702cf6e4bcd37d75fca3
Philip Damra <pdamra@ensomata.com>
2022-09-08 15:40:33 -0700

    App controller route for hooks

--- application/src/app.controller.ts
+++ application/src/app.controller.ts
@@ -1,14 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { LoggerService } from 'logger/logger.service';
 import { AppService } from './app.service';
 
 @ApiTags('HealthCheck')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly log: LoggerService
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('hooks/:hook')
+  handleHookRequest(
+    @Body() record: Record<string, unknown>,
+    @Param('hook') hookName: string
+  ) {
+    this.log.log(record, `${hookName} hook`);
+    return record;
   }
 }
